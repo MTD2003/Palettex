38102d18398a9f141263e332cb284cf53a16ffa2
TyeStates <155793960+TyeStates@users.noreply.github.com>
2025-03-10 14:06:16 -0300

    Create js outline
    Basic outline of a few js functions that we will need. lockBlock has check for class that does not exist yet on buttons, but can be easily added.

---
+++ GenScript.js
@@ -0,0 +1,20 @@
+function generatePalette(){
+    //
+}
+//<button onclick="lockBlock(this.id, "block1")"></button> example function call in html
+function lockBlock(ButtonId,BlockID){
+    if(document.getElementById(ButtonId).className == "unlocked"){ 
+        // set tag locked
+        // change button to closed lock
+    }
+    
+    else{
+        // set tag unlocked
+        // change button to open lock
+    }
+}
+
+function lookup(){
+    
+}
+
