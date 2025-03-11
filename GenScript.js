9d6d6db034927b130d9f941a8da551a723854769
TyeStates <155793960+TyeStates@users.noreply.github.com>
2025-03-11 17:06:02 -0300

    Add in lock function
    Made lock function which can lock or unlock blocks and will change the icon on the button. Had to put ids on images and buttons. ids are currently just buttonx, or blockx where x is the position from left to right.

--- GenScript.js
+++ GenScript.js
@@ -1,16 +1,34 @@
 function generatePalette(){
-    //
+    // will check if blocks have locked state, then randomize ones that do not.
 }
+
 //<button onclick="lockBlock(this.id, "block1")"></button> example function call in html
+/* sets block element to not be randomized in subsequent generations. */
 function lockBlock(ButtonId,BlockID){
-    if(document.getElementById(ButtonId).className == "unlocked"){ 
-        // set tag locked
+    let button = document.getElementById(ButtonId);
+    let block = document.getElementById(BlockID);
+    let i_element = button.firstElementChild; // takes the <i> element from the button found at buttonId
+    if(i_element.classList.contains("fa-lock-open")){ 
+        block.classList.add("locked"); // add locked state
+        
         // change button to closed lock
+        i_element.classList.remove("fa-lock-open"); 
+        i_element.classList.add("fa-lock"); 
+        return;
     }
     
-    else{
-        // set tag unlocked
+    else if(i_element.classList.contains("fa-lock")){ 
+        block.classList.remove("locked"); // remove locked state
+
         // change button to open lock
+        i_element.classList.remove("fa-lock"); 
+        i_element.classList.add("fa-lock-open");
+        return;
+    }
+
+    else{ // should never occur
+        alert("error, invalid button class.");
+        return;
     }
 }
 
