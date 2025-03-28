2c5c217c45fcde4cb7ecb039b05ff6229976cd19
Duncan <118747708+DuncanAcad@users.noreply.github.com>
2025-03-28 08:02:23 -0300

    Suppress unused warning for RedirectController

--- src/main/java/palettex/RedirectController.java
+++ src/main/java/palettex/RedirectController.java
@@ -3,6 +3,7 @@ package palettex;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+@SuppressWarnings("unused")
 @Controller
 public class RedirectController {
     @GetMapping("/")
