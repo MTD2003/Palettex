db8497658f944b4ef1d9a7266ab7f1934a4a6ed8
Michael <michaeltimothydoherty003@gmail.com>
2025-03-30 15:41:20 -0300

    Palettes now dynamically fill page palettes.html
    - Favourites button accurately reflects status in favourites bar.
    - TODO: Proper show more button.

--- src/main/resources/static/headerscript.js
+++ src/main/resources/static/headerscript.js
@@ -1,6 +1,6 @@
 // Loads the header and footer for each file on page load.
 // Requires the use of jQuery. Not a costly requirement as our database requires jQuery calls.
 $(function() {
-    $("#header").load("header.html"); 
-    $("#footer").load("footer.html");
+    $("#header").load("reusable/header.html"); 
+    $("#footer").load("reusable/footer.html");
 });
--- src/main/resources/static/pscript.js
+++ src/main/resources/static/pscript.js
@@ -5,13 +5,13 @@ function faveSelect(faveID) {
     const button = document.getElementById(faveID);
     const parent = button.parentElement;
     const texcode = parseInt(parent.getAttribute("id"));
-    if(parent.classList.contains("faved")) {
-        parent.classList.remove("faved");
+    if(button.classList.contains("faved")) {
+        button.classList.remove("faved");
         removeFavouritePalette(texcode);
 
         button.innerHTML = "Favourite <i class=\"fa-regular fa-heart\"></i>";
     } else {
-        parent.classList.add("faved");
+        button.classList.add("faved");
         addFavouritePalette(texcode);
 
         button.innerHTML = "Unfavourite <i class=\"fa-solid fa-heart\"></i>";
@@ -40,3 +40,55 @@ function deleteItem(tabId){
     const table = document.getElementById("SPT");
     
 }
+
+async function fillPalettes(lastPalette) {
+    const parse = lastPalette.split(":"); // Need to parse the stringify ourselves as JSON.parse doesn't work.
+    let sCode = parseInt(parse[2]);
+
+    const endCode = Math.max(0, sCode - 12);
+    const spt = document.getElementById("spt"); // SPT stands for saved palette tables, encompassing box.
+    let newHTML = ``;
+    let favBtn = ``;
+
+    for(; sCode > endCode; sCode--) {
+        // Handling favourite button/favourites.
+        if(isFavouritePalette(sCode)) {
+            favBtn = `
+            <button id="${sCode}-btn" class="btn btn-danger mt-2 faved" style="width: 90%;" onclick="faveSelect(this.id)">
+                Unfavourite <i class="fa-solid fa-heart"></i>
+            </button>
+            `;
+        } else {
+            favBtn = `
+            <button id="${sCode}-btn" class="btn btn-danger mt-2" style="width: 90%;" onclick="faveSelect(this.id)">
+                Favourite <i class="fa-regular fa-heart"></i>
+            </button>
+            `;
+        }
+
+        // Putting the html into the spt with proper textures.
+        apiGetPalette(sCode,
+            (data) => {
+                let tblock = data["blocks"];
+                spt.innerHTML += `
+                <div id="${sCode}" class="col-3 text-center rounded bg-info m-4 py-3">
+                    <h3 class="fs-4 py-1">Palette #${sCode}</h3>
+                    <img src="img/textures/${tblock[0]}.png" alt="Placeholder" class="squeeze"><img src="img/textures/${tblock[1]}.png" alt="Placeholder" class="squeeze"><br>
+                    <img src="img/textures/${tblock[2]}.png" alt="Placeholder" class="squeeze"><img src="img/textures/${tblock[3]}.png" alt="Placeholder" class="squeeze">
+                    ${favBtn}
+                </div>
+                `;
+            },
+            (err) => window.alert("Error: " + err.responseText)
+        );
+    }
+
+    // TODO: Show more button implementation here.
+}
+
+window.addEventListener("load", (event) => {
+    apiGetLatestPalette(
+        (data) => fillPalettes(JSON.stringify(data)),
+        (err) => window.alert("Error: " + err.responseText)
+    );
+});
