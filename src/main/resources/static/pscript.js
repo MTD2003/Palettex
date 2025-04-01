3b8a25033e53cef3eda33131cf8bd30660104e7a
Duncan <118747708+DuncanAcad@users.noreply.github.com>
2025-04-01 11:04:26 -0300

    Introduce new variable for pscript loop
    might solve Palette #0 related issues

--- src/main/resources/static/pscript.js
+++ src/main/resources/static/pscript.js
@@ -20,11 +20,9 @@ function faveSelect(faveID) {
 
 // Fills the palettes page. Called on page load.
 // Takes into account the "start" parameter using URLSearchParams.get
-async function fillPalettes(lastPalette) {
-    const parse = lastPalette.split(":"); // Need to parse the stringify ourselves as JSON.parse doesn't work.
+async function fillPalettes(sCode) {
     const urlParams = new URLSearchParams(window.location.search);
     const newStart = urlParams.get("start"); // Gets the start parameter from the URL.
-    let sCode = parseInt(parse[2]);
 
     if(newStart) { // If the start value is valid, replace sCode with it (unless it is too high).
         sCode = Math.min(sCode, newStart);
@@ -34,29 +32,29 @@ async function fillPalettes(lastPalette) {
     const spt = document.getElementById("spt"); // SPT stands for saved palette tables, encompassing box.
     let favBtn = ``;
 
-    for(; sCode > endCode; sCode--) {
+    for(let i = sCode; i > endCode; i--) {
         // Handling favourite button/favourites.
-        if(isFavouritePalette(sCode)) {
+        if(isFavouritePalette(i)) {
             favBtn = `
-            <button id="${sCode}-btn" class="btn btn-danger mt-2 faved" style="width: 90%;" onclick="faveSelect(this.id)">
+            <button id="${i}-btn" class="btn btn-danger mt-2 faved" style="width: 90%;" onclick="faveSelect(this.id)">
                 Unfavourite <i class="fa-solid fa-heart"></i>
             </button>
             `;
         } else {
             favBtn = `
-            <button id="${sCode}-btn" class="btn btn-danger mt-2" style="width: 90%;" onclick="faveSelect(this.id)">
+            <button id="${i}-btn" class="btn btn-danger mt-2" style="width: 90%;" onclick="faveSelect(this.id)">
                 Favourite <i class="fa-regular fa-heart"></i>
             </button>
             `;
         }
 
         // Putting the html into the spt with proper textures.
-        apiGetPalette(sCode,
+        apiGetPalette(i,
             (data) => {
                 let tblock = data["blocks"];
                 spt.innerHTML += `
-                <div id="${sCode}" class="col-3 text-center rounded bg-info m-4 py-3">
-                    <h3 class="fs-4 py-1">Palette #${sCode}</h3>
+                <div id="${i}" class="col-3 text-center rounded bg-info m-4 py-3">
+                    <h3 class="fs-4 py-1">Palette #${i}</h3>
                     <img src="img/textures/${tblock[0]}.png" alt="${tblock[0]}" class="squeeze"><img src="img/textures/${tblock[1]}.png" alt="${tblock[1]}" class="squeeze"><br>
                     <img src="img/textures/${tblock[2]}.png" alt="${tblock[2]}" class="squeeze"><img src="img/textures/${tblock[3]}.png" alt="${tblock[3]}" class="squeeze">
                     ${favBtn}
@@ -68,7 +66,7 @@ async function fillPalettes(lastPalette) {
         await new Promise(resolve => setTimeout(resolve, 70)); // Best way I could find to ensure the loops were synchronized.
     }
     
-    if(sCode > 0) { // Add the show more button.
+    if(endCode > 0) { // Add the show more button.
         document.getElementById("next").innerHTML += `
         <form class="text-center" action="palettes.html" method="get">
             <input type="hidden" name="start" value="${sCode}">
@@ -80,7 +78,7 @@ async function fillPalettes(lastPalette) {
 
 window.addEventListener("load", (event) => {
     apiGetLatestPalette(
-        (data) => fillPalettes(JSON.stringify(data)),
+        (data) => fillPalettes(data['code']),
         (err) => window.alert("Error: " + err.responseText)
     );
 });
